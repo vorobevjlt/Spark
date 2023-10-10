0af29b65db380f97117d4f42778b3436c41b1f7a
Evgenii Vorobev <jlt540e@gmail.com>
2023-10-10 09:30:28 +0000

    change 6.5.5

---
+++ src/main/scala/constants/ConstantPath.scala
@@ -0,0 +1,29 @@
+package constants
+
+import org.apache.spark.sql.functions._
+
+object ConstantPath {
+
+    val HasReadPath = Array(
+        "src/main/resources/dataArchiveForRead/airports.csv",
+        "src/main/resources/dataArchiveForRead/flights.csv",
+        "src/main/resources/dataArchiveForRead/airlines.csv",
+        "src/main/resources/dataArchiveForRead/TopAirlines",
+        "src/main/resources/dataArchiveForRead/TopAirport",
+        "src/main/resources/dataArchiveForRead/TopFlyInOneDirectionByAirport",
+        "src/main/resources/dataArchiveForRead/TopWeekDaysByArrivalDelay",
+        "src/main/resources/dataArchiveForRead/CountDelayReason",
+        "src/main/resources/dataArchiveForRead/PercentageDelayReason",
+        "src/main/resources/dataArchiveForRead/MetaInfo"
+    )
+
+    val HasWritePath = Array(
+        "src/main/resources/dataArchiveForWrite/TopAirlines",
+        "src/main/resources/dataArchiveForWrite/TopAirport",
+        "src/main/resources/dataArchiveForWrite/TopFlyInOneDirectionByAirport",
+        "src/main/resources/dataArchiveForWrite/TopWeekDaysByArrivalDelay",
+        "src/main/resources/dataArchiveForWrite/CountDelayReason",
+        "src/main/resources/dataArchiveForWrite/PercentageDelayReason",
+        "src/main/resources/dataArchiveForWrite/MetaInfo"
+    )    
+}
