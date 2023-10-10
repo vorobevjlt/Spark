d5c8d74ab531c9e6d19ca01f25a1bf62cffbf38b
Evgenii Vorobev <jlt540e@gmail.com>
2023-10-10 09:26:19 +0000

    change 6.5.5

--- src/main/scala/FlightAnalyzer.scala
+++ src/main/scala/FlightAnalyzer.scala
@@ -1,20 +1,15 @@
 package com.example
 
 import configs.Config
-import org.apache.spark.sql.{DataFrame, SparkSession}
-import org.apache.spark.sql.types._
-import readers.DataFrameReader
-import schemas.Schema
-import writers.DataFrameWriter
-import jobs.{Job, JobConfig, FlyghtAnalysisJob}
 
-object FlightAnalyzer extends Config with SessionWrapper {
+object FlightAnalyzer extends Config {
 
-  val file = Array("a")
-
-  def main(args: String): Unit = {
-    Config(file)
-  }
-
-}  
-
+  def main(args: Array[String]): Unit = {
+    
+    args match {
+      case items if (items.length == 10 || items.length == 0) => Config(args)
+      case _ => throw new IllegalArgumentException(s"Not enough files, have to be 10 but ${args.length} found")
+    }
+    
+  }  
+}
--- src/main/scala/configs/Config.scala
+++ src/main/scala/configs/Config.scala
@@ -1,222 +1,124 @@
 package configs
 
+import constants.ConstantPath
+import jobs.{Job, JobConfig, FlyghtAnalysisJob}
 import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.spark.sql.types._
+import writers.DataFrameWriter
 import readers.DataFrameReader
 import schemas.Schema
-import writers.DataFrameWriter
-import jobs.{Job, JobConfig, FlyghtAnalysisJob}
 
 trait Config {
     val readHeader: Boolean = true
     val setSeparator: Char = ','
-    // val env = sys.env("HOME")
-    val env = "/root"
-    val readPath = "src/main/resources/dataForRead/"
-    val readArchive = "src/main/resources/dataArchiveForRead/"
+    val localPath = ConstantPath.HasReadPath
+    val writePath = ConstantPath.HasWritePath
+    val writeFormat = "parquet"
 }
 
 object Config extends Config with SessionWrapper{
-    private object LocalConfig extends Config {
-         
-            val job = new FlyghtAnalysisJob(
-                spark,
-                JobConfig(
-                DataFrameReader.ReadConfig(
-                    file = s"$readPath airports.csv}",
-                    schema = Schema.schemaAirport,
-                    hasHeader = readHeader,
-                    separator = setSeparator),
-
-                DataFrameReader.ReadConfig(
-                    file = s"${readPath}flights.csv",
-                    schema = Schema.schemaFly,
-                    hasHeader = readHeader,
-                    separator = setSeparator),
-
-                DataFrameReader.ReadConfig(
-                    file = s"${readPath}airlines.csv",
-                    schema = Schema.schemaAirline,
-                    hasHeader = readHeader,
-                    separator = setSeparator),
-
-                DataFrameReader.ReadConfig(
-                    file = s"${readArchive}TopAirlines",
-                    schema = Schema.schemaTopAirline,
-                    hasHeader = readHeader,
-                    separator = setSeparator),
-
-                DataFrameReader.ReadConfig(
-                    file = s"${readArchive}TopAirport",
-                    schema = Schema.schemaTopAirport,
-                    hasHeader = readHeader,
-                    separator = setSeparator),
-
-                DataFrameReader.ReadConfig(
-                    file = s"${readArchive}TopFlyInOneDirectionByAirport",
-                    schema = Schema.schemaTopFlyInOneDirection,
-                    hasHeader = readHeader,
-                    separator = setSeparator),
-
-                DataFrameReader.ReadConfig(
-                    file = s"${readArchive}TopWeekDaysByArrivalDelay",
-                    schema = Schema.schemaTopWeekDays,
-                    hasHeader = readHeader,
-                    separator = setSeparator),
-
-                DataFrameReader.ReadConfig(
-                    file = s"${readArchive}CountDelayReason",
-                    schema = Schema.schemaCountDelayReason,
-                    hasHeader = readHeader,
-                    separator = setSeparator),
-
-                DataFrameReader.ReadConfig(
-                    file = s"${readArchive}PercentageDelayReason",
-                    schema = Schema.schemaPercentageDelayReason,
-                    hasHeader = readHeader,
-                    separator = setSeparator),
-
-                DataFrameReader.ReadConfig(
-                    file = s"${readArchive}MetaInfo",
-                    schema = Schema.schemaMetaInfo,
-                    hasHeader = false,
-                    separator = setSeparator),
-
-                DataFrameWriter.WriteConfig(
-                    outputFile = "src/main/resources/dataArchiveForWrite/TopAirport",
-                    format = "parquet"),
-
-                DataFrameWriter.WriteConfig(
-                    outputFile = "src/main/resources/dataArchiveForWrite/TopAirlines",
-                    format = "parquet"),
-
-                DataFrameWriter.WriteConfig(
-                    outputFile = "src/main/resources/dataArchiveForWrite/TopFlyInOneDirectionByAirport",
-                    format = "parquet"),
-
-                DataFrameWriter.WriteConfig(
-                    outputFile = "src/main/resources/dataArchiveForWrite/TopWeekDaysByArrivalDelay",
-                    format = "parquet"),
-
-                DataFrameWriter.WriteConfig(
-                    outputFile = "src/main/resources/dataArchiveForWrite/CountDelayReason",
-                    format = "parquet"),
-
-                DataFrameWriter.WriteConfig(
-                    outputFile = "src/main/resources/dataArchiveForWrite/PercentageDelayReason",
-                    format = "parquet"),
-
-                DataFrameWriter.WriteConfig(
-                    outputFile = "src/main/resources/dataArchiveForWrite/MetaInfo",
-                    format = "csv")
-                )
-                )
-            job.run()
-        }
-
-        // private object ClasterConfig extends Config {
-        //         val job = new FlyghtAnalysisJob(
-        //         spark,
-        //         JobConfig(
-        //             DataFrameReader.ReadConfig(
-        //             file = args(0),
-        //             schema = Schema.schemaAirport,
-        //             hasHeader = true,
-        //             separator = ','),
-
-        //             DataFrameReader.ReadConfig(
-        //             file = args(1),
-        //             schema = Schema.schemaFly,
-        //             hasHeader = true,
-        //             separator = ','),
-
-        //             DataFrameReader.ReadConfig(
-        //             file = args(2),
-        //             schema = Schema.schemaAirline,
-        //             hasHeader = true,
-        //             separator = ','),
-
-        //             DataFrameReader.ReadConfig(
-        //             file = args(3),
-        //             schema = Schema.schemaTopAirline,
-        //             hasHeader = true,
-        //             separator = ','),
-
-        //             DataFrameReader.ReadConfig(
-        //             file = args(4),
-        //             schema = Schema.schemaTopAirport,
-        //             hasHeader = true,
-        //             separator = ','),
-
-        //             DataFrameReader.ReadConfig(
-        //             file = args(5),
-        //             schema = Schema.schemaTopFlyInOneDirection,
-        //             hasHeader = true,
-        //             separator = ','),
-
-        //             DataFrameReader.ReadConfig(
-        //             file = args(6),
-        //             schema = Schema.schemaTopWeekDays,
-        //             hasHeader = true,
-        //             separator = ','),
-
-        //             DataFrameReader.ReadConfig(
-        //             file = args(7),
-        //             schema = Schema.schemaCountDelayReason,
-        //             hasHeader = true,
-        //             separator = ','),
-
-        //             DataFrameReader.ReadConfig(
-        //             file = args(8),
-        //             schema = Schema.schemaPercentageDelayReason,
-        //             hasHeader = true,
-        //             separator = ','),
-
-        //         DataFrameReader.ReadConfig(
-        //             file = args(9),
-        //             schema = Schema.schemaMetaInfo,
-        //             hasHeader = false,
-        //             separator = ','),
-
-        //             DataFrameWriter.WriteConfig(
-        //             outputFile = "/opt/spark-data/dataArchiveForWrite/TopAirport",
-        //             format = "parquet"),
-
-        //             DataFrameWriter.WriteConfig(
-        //             outputFile = "/opt/spark-data/dataArchiveForWrite/TopAirlines",
-        //             format = "parquet"),
-
-        //             DataFrameWriter.WriteConfig(
-        //             outputFile = "/opt/spark-data/dataArchiveForWrite/TopFlyInOneDirectionByAirport",
-        //             format = "parquet"),
-
-        //             DataFrameWriter.WriteConfig(
-        //             outputFile = "/opt/spark-data/dataArchiveForWrite/TopWeekDaysByArrivalDelay",
-        //             format = "parquet"),
-
-        //             DataFrameWriter.WriteConfig(
-        //             outputFile = "/opt/spark-data/dataArchiveForWrite/CountDelayReason",
-        //             format = "parquet"),
-
-        //             DataFrameWriter.WriteConfig(
-        //             outputFile = "/opt/spark-data/dataArchiveForWrite/PercentageDelayReason",
-        //             format = "parquet"),
-
-        //             DataFrameWriter.WriteConfig(
-        //             outputFile = "/opt/spark-data/dataArchiveForWrite/MetaInfo",
-        //             format = "csv")
-        //             ) 
-        //         )
-        //         job.run()
-        // }
-
-    def apply(
-            args: Array[String],
-            envType: String = env): Unit = {
+    def apply(args: Array[String] = Array()): Unit = {
         args match {
-            case name if (name.nonEmpty && envType == "/root") => LocalConfig(name)
-            case name if (name.isEmpty && envType != "/root") => println("root")
-            case _ => throw new IllegalArgumentException(s"Invalid")
+            case rec if(rec.nonEmpty) => RunConfig(args)
+            case _ => RunConfig(localPath)       
+        }
+    }
+
+    private object RunConfig extends Config {
+        def apply(readPath: Array[String]): Unit = {
+            val job = new FlyghtAnalysisJob(
+                        spark,
+                        JobConfig(
+                        DataFrameReader.ReadConfig(
+                            file = readPath(0),
+                            schema = Schema.schemaAirport,
+                            hasHeader = readHeader,
+                            separator = setSeparator),
+
+                        DataFrameReader.ReadConfig(
+                            file = readPath(1),
+                            schema = Schema.schemaFly,
+                            hasHeader = readHeader,
+                            separator = setSeparator),
+
+                        DataFrameReader.ReadConfig(
+                            file = readPath(2),
+                            schema = Schema.schemaAirline,
+                            hasHeader = readHeader,
+                            separator = setSeparator),
+
+                        DataFrameReader.ReadConfig(
+                            file = readPath(3),
+                            schema = Schema.schemaTopAirline,
+                            hasHeader = readHeader,
+                            separator = setSeparator),
+
+                        DataFrameReader.ReadConfig(
+                            file = readPath(4),
+                            schema = Schema.schemaTopAirport,
+                            hasHeader = readHeader,
+                            separator = setSeparator),
+
+                        DataFrameReader.ReadConfig(
+                            file = readPath(5),
+                            schema = Schema.schemaTopFlyInOneDirection,
+                            hasHeader = readHeader,
+                            separator = setSeparator),
+
+                        DataFrameReader.ReadConfig(
+                            file = readPath(6),
+                            schema = Schema.schemaTopWeekDays,
+                            hasHeader = readHeader,
+                            separator = setSeparator),
+
+                        DataFrameReader.ReadConfig(
+                            file = readPath(7),
+                            schema = Schema.schemaCountDelayReason,
+                            hasHeader = readHeader,
+                            separator = setSeparator),
+
+                        DataFrameReader.ReadConfig(
+                            file = readPath(8),
+                            schema = Schema.schemaPercentageDelayReason,
+                            hasHeader = readHeader,
+                            separator = setSeparator),
+
+                        DataFrameReader.ReadConfig(
+                            file = readPath(9),
+                            schema = Schema.schemaMetaInfo,
+                            hasHeader = false,
+                            separator = setSeparator),
+
+                        DataFrameWriter.WriteConfig(
+                            outputFile = writePath(0),
+                            format = writeFormat),
+
+                        DataFrameWriter.WriteConfig(
+                            outputFile = writePath(1),
+                            format = writeFormat),
+
+                        DataFrameWriter.WriteConfig(
+                            outputFile = writePath(2),
+                            format = writeFormat),
+
+                        DataFrameWriter.WriteConfig(
+                            outputFile = writePath(3),
+                            format = writeFormat),
+
+                        DataFrameWriter.WriteConfig(
+                            outputFile = writePath(4),
+                            format = writeFormat),
+
+                        DataFrameWriter.WriteConfig(
+                            outputFile = writePath(5),
+                            format = writeFormat),
+
+                        DataFrameWriter.WriteConfig(
+                            outputFile = writePath(6),
+                            format = "csv")
+                        )
+            )
+            job.run()
         }
     }
 }
